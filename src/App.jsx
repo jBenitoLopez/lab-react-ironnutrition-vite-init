6711087ff1dd5e800d011af4f43aa565388980f1
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-12 17:15:14 +0200

    add: iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import "./App.css";
-import foodsJson from "./foods.json";
-import React, {useState} from 'react';
+import "./App.css"
+import FoodBox from './components/FoodBox'
+import foodsJson from "./foods.json"
+import React, { useState } from 'react'
 
 function App() {
 
   const [foods, setFoods] = useState([...foodsJson])
 
-  console.log('foods :>> ', foods);
+  console.log('foods :>> ', foods)
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {/* {
+          foods.map((food)=><FoodBox food={food}/>)
+        } */}
+      <FoodBox food={{
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }}
+      />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,25 @@
-// Your code here
+import React from 'react'
+
+function FoodBox({food}) {
+
+  const {id, name, calories, image, servings} = food
+
+  return (
+    <div key={id}>
+      <h2 className='text-3xl font-bold m-6'>{name}</h2>
+
+      <img className='mx-auto' src={image} />
+
+      <p className='text-lg mt-4'>Calories: {calories}</p>
+      <p className='text-lg mt-2'>Servings {servings}</p>
+
+      <p className='mt-4'>
+        <b>Total Calories: {servings * calories}</b> kcal
+      </p>
+
+      <button className='text-2xl font-bold mt-4'>Delete</button>
+    </div>
+  )
+}
+
+export default FoodBox
