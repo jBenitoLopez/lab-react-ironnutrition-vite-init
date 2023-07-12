4207bec1ab689cd7233cd14978f2fd7b8db4293a
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-12 22:29:25 +0200

    install: tailwind CSS and configure

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <div className="App">
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <h1>LAB | React IronNutrition</h1>
       <FoodList />
     </div>
