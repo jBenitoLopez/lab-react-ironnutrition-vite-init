7de7c9ac44620827b91b2926df3e07d234bc834a
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-12 16:55:40 +0200

    add: Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import React, {useState} from 'react';
 
 function App() {
+
+  const [foods, setFoods] = useState([...foodsJson])
+
+  console.log('foods :>> ', foods);
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
