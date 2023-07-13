b7ea995a2124f32bcf79ab0aaa9bfc0900e85df0
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-13 14:51:28 +0200

    fix css search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,11 @@
 function Search({ handlerFilter }) {
   return (
-    <div className='mt-5 relative flex flex-col text-sky-400/100 justify-center overflow-hidden'>
-      <div className='w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl'>
+    <div className='mt-5 relative flex flex-col text-fuchsia-700 justify-center overflow-hidden'>
+      <div className='w-full p-6 m-auto bg-fuchsia-100 rounded-md shadow-md lg:max-w-xl'>
         <h2 className='text-4xl font-bold mb-6'>Filter Food</h2>
 
         <div className="">
-          <input type="text" className="w-full input input-bordered input-primary" onChange={(e) => handlerFilter(e.target.value)} />
+          <input type="text" className="w-full input input-bordered input-primary text-fuchsia-200" onChange={(e) => handlerFilter(e.target.value)} />
         </div>
 
       </div>
