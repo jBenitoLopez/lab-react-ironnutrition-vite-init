ecff5a05ce02d3e0c6c8fe7b63551f2c1d6059f4
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-13 14:48:17 +0200

    component Search add CSS

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,17 @@
 function Search({ handlerFilter }) {
   return (
-    <div>
-      <h2 className='text-4xl font-bold mt-16 mb-8'>Filter Food</h2>
-      <div className="form-control">
-        <label className="input-group">
-          <input type="text" className="input input-bordered w-96" onChange={(e) => handlerFilter(e.target.value)} />
-        </label>
-      </div>
+    <div className='mt-5 relative flex flex-col text-sky-400/100 justify-center overflow-hidden'>
+      <div className='w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl'>
+        <h2 className='text-4xl font-bold mb-6'>Filter Food</h2>
 
+        <div className="">
+          <input type="text" className="w-full input input-bordered input-primary" onChange={(e) => handlerFilter(e.target.value)} />
+        </div>
+
+      </div>
     </div>
   )
 }
 
 export default Search
+
